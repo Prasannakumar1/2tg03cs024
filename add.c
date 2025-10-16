81328e64a8194ec1b4b8e82094de6a66d7147e98
Prasannakumar1 <pkshivaraddi@gmail.com>
2025-10-16 15:24:38 +0530

    Addition 2 nors also added

--- add.c
+++ add.c
@@ -2,7 +2,10 @@
 int add(int x, int y, int z){
 	return x+y+z;
 }
+int add(int x, int y){
+return x+y;
+}
 void main(){
    printf("Addition 0f 3 nors=%d",add(2,3,4));
-
-}
+   printf("Addition of 2 nors=%d",add(2,3
+		   }
